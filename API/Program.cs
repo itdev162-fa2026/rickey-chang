8979d86df5cf783e231c3dab745f343a07721685
Rickey Chang <changr1@gmatc.matc.edu>
2025-11-01 18:56:41 -0500

    Complete Activity 8: Shopping Cart & Product Search

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,17 @@
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+    policy =>
+    {
+        policy.WithOrigins("http://localhost:5174")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 // Add service to the container.
 builder.Services.AddControllers();
@@ -11,6 +22,9 @@ builder.Services.AddDbContext<DataContext>();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+// Use CORS
+app.UseCors("AllowReactApp");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
